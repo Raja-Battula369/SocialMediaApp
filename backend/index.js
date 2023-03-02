8665a64038ba01d9cbd0b2d051b0c0dabec09cfc
Raja-Battula369 <battulavenkatamuralikrishnaraj@gmail.com>
2023-03-02 23:25:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,19 @@
-const DataBase=require('./DataBase/Db');
-const dotenv=require('dotenv');
+const DataBase = require('./DataBase/Db');
+const dotenv = require('dotenv');
 const app = require('./app');
-const {posts,users}=require('./data/index');
-const Post=require('./models/postsModel');
-const User=require('./models/userModel');
+const { posts, users } = require('./data/index');
+const Post = require('./models/postsModel');
+const User = require('./models/userModel');
 
 dotenv.config();
 
-const DB=process.env.DATABASE;
-const PORT=process.env.PORT;
+const DB = process.env.DATABASE;
+const PORT = process.env.PORT;
 
-const server=async()=> {
+const server = async () => {
     await DataBase(DB);
-    // await Post.insertMany(posts);
-    // await User.insertMany(users);
-    app.listen(PORT,()=> {
+
+    app.listen(PORT, () => {
         console.log('Server listening....!');
     });
 };
