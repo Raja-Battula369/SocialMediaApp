361cb73329f6f1e2f5a43b7f1d74b7b572f46d1a
Raja-Battula369 <battulavenkatamuralikrishnaraj@gmail.com>
2023-04-16 09:36:03 +0530

    fixedAbug

--- frontend/src/components/HomePage/Posts/Comments/Comment.jsx
+++ frontend/src/components/HomePage/Posts/Comments/Comment.jsx
@@ -13,7 +13,7 @@ const Comment = ({ id, message }) => {
   const commentsPersonId = async () => {
     try {
       const { data } = await axios.get(
-        `https://socialmediaapp-9air.onrender.com/posts/${id}`,
+        `https://socialmediaapp-9air.onrender.com/users/${id}`,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
