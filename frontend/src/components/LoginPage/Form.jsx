728e15460562496e66b1d141a00e4595438965a5
Raja-Battula369 <battulavenkatamuralikrishnaraj@gmail.com>
2023-04-25 16:59:57 +0530

    bugFixed

--- frontend/src/components/LoginPage/Form.jsx
+++ frontend/src/components/LoginPage/Form.jsx
@@ -83,6 +83,12 @@ const Form = () => {
 
   const login = async (values, onSubmitProps = '') => {
     try {
+      toast({
+        title: 'Please wait for 30sec, backend Server to Load ',
+        status: 'loading',
+        isClosable: true,
+        duration: 10000,
+      });
       setIsLoading(true);
       const data = await RouterFetchForPost('/auth/login', values);
 
@@ -137,259 +143,249 @@ const Form = () => {
 
   return (
     <>
-      {isLoading ? (
-        <News />
-      ) : (
-        <Formik
-          onSubmit={handleFormSubmit}
-          initialValues={isLogin ? intialValuesLogin : initialValuesRegister}
-          validationSchema={isLogin ? loginSchema : registerSchema}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            setFieldValue,
-            resetForm,
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <VStack gap={'1rem'} shadow="md" p="1rem" className="login-card">
-                {isRegister && (
-                  <>
-                    <SimpleGrid
-                      columns={{ sm: 1, md: 2 }}
-                      spacing={10}
-                      color={'white'}
-                    >
-                      <FormControl
-                        isInvalid={touched.firstName && errors.firstName}
-                      >
-                        <Input
-                          appearance={'none'}
-                          title="firstName"
-                          type={'text'}
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          name="firstName"
-                          placeholder="firstName"
-                        />
-                        <FormErrorMessage>
-                          *{touched.firstName && errors.firstName}
-                        </FormErrorMessage>
-                      </FormControl>
-
-                      <FormControl
-                        isInvalid={touched.lastName && errors.lastName}
-                      >
-                        <Input
-                          appearance={'none'}
-                          title="lastName"
-                          type={'text'}
-                          onChange={handleChange}
-                          onBlur={handleBlur}
-                          name="lastName"
-                          placeholder="lastName"
-                        />
-                        <FormErrorMessage>
-                          *{touched.lastName && errors.lastName}
-                        </FormErrorMessage>
-                      </FormControl>
-                    </SimpleGrid>
-
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={isLogin ? intialValuesLogin : initialValuesRegister}
+        validationSchema={isLogin ? loginSchema : registerSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          setFieldValue,
+          resetForm,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <VStack gap={'1rem'} shadow="md" p="1rem" className="login-card">
+              {isRegister && (
+                <>
+                  <SimpleGrid
+                    columns={{ sm: 1, md: 2 }}
+                    spacing={10}
+                    color={'white'}
+                  >
                     <FormControl
-                      isInvalid={touched.location && errors.location}
+                      isInvalid={touched.firstName && errors.firstName}
                     >
                       <Input
                         appearance={'none'}
-                        title="loc"
+                        title="firstName"
                         type={'text'}
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        name="location"
-                        placeholder="location"
+                        name="firstName"
+                        placeholder="firstName"
                       />
                       <FormErrorMessage>
-                        *{touched.location && errors.location}
+                        *{touched.firstName && errors.firstName}
                       </FormErrorMessage>
                     </FormControl>
 
                     <FormControl
-                      isInvalid={touched.occupation && errors.occupation}
+                      isInvalid={touched.lastName && errors.lastName}
                     >
                       <Input
                         appearance={'none'}
-                        title="occ"
+                        title="lastName"
                         type={'text'}
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        name="occupation"
-                        placeholder="occupation"
+                        name="lastName"
+                        placeholder="lastName"
                       />
                       <FormErrorMessage>
-                        *{touched.occupation && errors.occupation}
+                        *{touched.lastName && errors.lastName}
                       </FormErrorMessage>
                     </FormControl>
+                  </SimpleGrid>
 
-                    <Box w={'full'}>
-                      <Dropzone
-                        acceptedFiles=".jpg,.jpeg,.png"
-                        multiple={false}
-                        accept={{ 'image/*': [] }}
-                        onDrop={(acceptedFiles) =>
-                          setFieldValue('picture', acceptedFiles[0])
-                        }
-                      >
-                        {({ getRootProps, getInputProps }) => (
-                          <Box
-                            w={'full'}
-                            {...getRootProps()}
-                            border={'2px dashed grey'}
-                            p="1rem"
-                            sx={{ '&:hover': { cursor: 'pointer' } }}
-                          >
-                            <input
-                              type="file"
-                              accept="image/apng, image/jpg, image/gif, image/jpeg, image/png, image/svg+xml, image/webp"
-                              appearance={'none'}
-                              title="input"
-                              {...getInputProps()}
-                            />
+                  <FormControl isInvalid={touched.location && errors.location}>
+                    <Input
+                      appearance={'none'}
+                      title="loc"
+                      type={'text'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="location"
+                      placeholder="location"
+                    />
+                    <FormErrorMessage>
+                      *{touched.location && errors.location}
+                    </FormErrorMessage>
+                  </FormControl>
 
-                            {!values.picture ? (
-                              <Text>Add Picture here</Text>
-                            ) : (
-                              <HStack justifyContent={'space-between'}>
-                                <Text>{values.picture.name}</Text>
-                                <MdOutlineModeEdit />
-                              </HStack>
-                            )}
-                          </Box>
-                        )}
-                      </Dropzone>
-                    </Box>
+                  <FormControl
+                    isInvalid={touched.occupation && errors.occupation}
+                  >
+                    <Input
+                      appearance={'none'}
+                      title="occ"
+                      type={'text'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="occupation"
+                      placeholder="occupation"
+                    />
+                    <FormErrorMessage>
+                      *{touched.occupation && errors.occupation}
+                    </FormErrorMessage>
+                  </FormControl>
 
-                    <FormControl isInvalid={touched.email && errors.email}>
-                      <Input
-                        appearance={'none'}
-                        title="email"
-                        type={'email'}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        name="email"
-                        placeholder="email"
-                      />
-                      <FormErrorMessage>
-                        *{touched.email && errors.email}
-                      </FormErrorMessage>
-                    </FormControl>
-
-                    <FormControl
-                      isInvalid={touched.password && errors.password}
+                  <Box w={'full'}>
+                    <Dropzone
+                      acceptedFiles=".jpg,.jpeg,.png"
+                      multiple={false}
+                      accept={{ 'image/*': [] }}
+                      onDrop={(acceptedFiles) =>
+                        setFieldValue('picture', acceptedFiles[0])
+                      }
                     >
-                      <Input
-                        appearance={'none'}
-                        title="pass"
-                        type={'password'}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        name="password"
-                        placeholder="password"
-                      />
-                      <FormErrorMessage>
-                        *{touched.password && errors.password}
-                      </FormErrorMessage>
-                    </FormControl>
-                  </>
-                )}
-                {isLogin && (
-                  <>
-                    <FormControl isInvalid={touched.email && errors.email}>
-                      <Input
-                        appearance={'none'}
-                        title="email1"
-                        type={'email'}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        name="email"
-                        placeholder="email"
-                      />
-                      <FormErrorMessage>
-                        *{touched.email && errors.email}
-                      </FormErrorMessage>
-                    </FormControl>
+                      {({ getRootProps, getInputProps }) => (
+                        <Box
+                          w={'full'}
+                          {...getRootProps()}
+                          border={'2px dashed grey'}
+                          p="1rem"
+                          sx={{ '&:hover': { cursor: 'pointer' } }}
+                        >
+                          <input
+                            type="file"
+                            accept="image/apng, image/jpg, image/gif, image/jpeg, image/png, image/svg+xml, image/webp"
+                            appearance={'none'}
+                            title="input"
+                            {...getInputProps()}
+                          />
 
-                    <FormControl
-                      isInvalid={touched.password && errors.password}
-                    >
-                      <Input
-                        appearance={'none'}
-                        title="pass2"
-                        type={'password'}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        name="password"
-                        placeholder="password"
-                      />
-                      <FormErrorMessage>
-                        *{touched.password && errors.password}
-                      </FormErrorMessage>
-                    </FormControl>
-                  </>
-                )}
-                <Button
-                  isLoading={isLoading}
-                  maxWidth={'full'}
-                  appearance={'none'}
-                  title="submit"
-                  type="submit"
-                >
-                  {isLogin ? 'LOGIN' : 'REGISTER'}
-                </Button>
-                <Button
-                  onClick={handleGuestLogin}
-                  w="full"
-                  variant={'unstyled'}
-                  border={'1px solid black'}
+                          {!values.picture ? (
+                            <Text>Add Picture here</Text>
+                          ) : (
+                            <HStack justifyContent={'space-between'}>
+                              <Text>{values.picture.name}</Text>
+                              <MdOutlineModeEdit />
+                            </HStack>
+                          )}
+                        </Box>
+                      )}
+                    </Dropzone>
+                  </Box>
+
+                  <FormControl isInvalid={touched.email && errors.email}>
+                    <Input
+                      appearance={'none'}
+                      title="email"
+                      type={'email'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="email"
+                      placeholder="email"
+                    />
+                    <FormErrorMessage>
+                      *{touched.email && errors.email}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl isInvalid={touched.password && errors.password}>
+                    <Input
+                      appearance={'none'}
+                      title="pass"
+                      type={'password'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="password"
+                      placeholder="password"
+                    />
+                    <FormErrorMessage>
+                      *{touched.password && errors.password}
+                    </FormErrorMessage>
+                  </FormControl>
+                </>
+              )}
+              {isLogin && (
+                <>
+                  <FormControl isInvalid={touched.email && errors.email}>
+                    <Input
+                      appearance={'none'}
+                      title="email1"
+                      type={'email'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="email"
+                      placeholder="email"
+                    />
+                    <FormErrorMessage>
+                      *{touched.email && errors.email}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl isInvalid={touched.password && errors.password}>
+                    <Input
+                      appearance={'none'}
+                      title="pass2"
+                      type={'password'}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="password"
+                      placeholder="password"
+                    />
+                    <FormErrorMessage>
+                      *{touched.password && errors.password}
+                    </FormErrorMessage>
+                  </FormControl>
+                </>
+              )}
+              <Button
+                isLoading={isLoading}
+                maxWidth={'full'}
+                appearance={'none'}
+                title="submit"
+                type="submit"
+              >
+                {isLogin ? 'LOGIN' : 'REGISTER'}
+              </Button>
+              <Button
+                onClick={handleGuestLogin}
+                w="full"
+                variant={'unstyled'}
+                border={'1px solid black'}
+                color={'white'}
+                bgColor={'black'}
+              >
+                Guest
+              </Button>
+              {isLogin ? (
+                <Text
+                  textDecoration={'underline'}
                   color={'white'}
-                  bgColor={'black'}
+                  sx={{ '&:hover': { cursor: 'pointer', opacity: 0.6 } }}
+                  fontSize={['0.5rem', '0.5rem', '0.7rem']}
+                  onClick={() => {
+                    setPageType(isLogin ? 'register' : 'login');
+                    resetForm();
+                  }}
                 >
-                  Guest
-                </Button>
-                {isLogin ? (
-                  <Text
-                    textDecoration={'underline'}
-                    color={'white'}
-                    sx={{ '&:hover': { cursor: 'pointer', opacity: 0.6 } }}
-                    fontSize={['0.5rem', '0.5rem', '0.7rem']}
-                    onClick={() => {
-                      setPageType(isLogin ? 'register' : 'login');
-                      resetForm();
-                    }}
-                  >
-                    Don't have an account? <Text as={'b'}>Sign Up here</Text>
-                  </Text>
-                ) : (
-                  <Text
-                    textDecoration={'underline'}
-                    color={'white'}
-                    sx={{ '&:hover': { cursor: 'pointer', opacity: 0.6 } }}
-                    fontSize={['0.5rem', '0.5rem', '0.7rem']}
-                    onClick={() => {
-                      setPageType(isLogin ? 'register' : 'login');
-                      resetForm();
-                    }}
-                  >
-                    Already have an account?<Text as={'b'}>Login here</Text>
-                  </Text>
-                )}
-              </VStack>
-            </form>
-          )}
-        </Formik>
-      )}
+                  Don't have an account? <Text as={'b'}>Sign Up here</Text>
+                </Text>
+              ) : (
+                <Text
+                  textDecoration={'underline'}
+                  color={'white'}
+                  sx={{ '&:hover': { cursor: 'pointer', opacity: 0.6 } }}
+                  fontSize={['0.5rem', '0.5rem', '0.7rem']}
+                  onClick={() => {
+                    setPageType(isLogin ? 'register' : 'login');
+                    resetForm();
+                  }}
+                >
+                  Already have an account?<Text as={'b'}>Login here</Text>
+                </Text>
+              )}
+            </VStack>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
